15ee6eb004957cffdd9bf3a8dd4c2d5ad69dd668
Zeprus <zeprusaliastrylon@googlemail.com>
2022-07-01 22:31:45 +0200

    Improved hook clarity, changed file structure for thunderstore

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -15,14 +15,14 @@ namespace AssetReplacer
         // Register config settings
         public static ConfigEntry<bool> ConfigEnableAllMods;
         public static ConfigEntry<bool> ConfigEnableTextureMods;
-        public static ConfigEntry<bool> ConfigEnableMusicMods;
+        public static ConfigEntry<bool> ConfigEnableAudioMods;
         public static ConfigEntry<string> ConfigTextureModFolders;
         public static ConfigEntry<string> ConfigMusicModFolders;
         public static ConfigEntry<bool> ConfigTextureDynamicFogOfWar;
 
+        public static ConfigEntry<string> ConfigAudioModFolders;
         internal static Harmony hPatchTitleCursor;
-        internal static Harmony hPatchTitleStart;
-        internal static Harmony hPatchBattleStart;
+        internal static Harmony hPatchControllerStart;
         internal static Harmony hPatchAudioSource;
 
         private void Awake()
@@ -32,13 +32,13 @@ namespace AssetReplacer
             // Configs (args: section, key, default, description)
             ConfigEnableAllMods = Config.Bind<bool>("All", "EnableAll", true, "Set to true to enable all mods, false to completely disable them");
             ConfigEnableTextureMods = Config.Bind<bool>("Textures", "EnableTextureMods", true, "Set to true to enable texture mods, false to completely disable them");
-            ConfigEnableMusicMods = Config.Bind<bool>("Music", "EnableMusicMods", true, "Set to true to enable music mods, false to completely disable them");
-            ConfigMusicModFolders = Config.Bind<string>("Music", "MusicModFolders", "Demo",
-            @"Name of the active music mod folder(s)
+            ConfigEnableAudioMods = Config.Bind<bool>("Audio", "EnableAudioMods", true, "Set to true to enable audio mods, false to completely disable them");
+            ConfigAudioModFolders = Config.Bind<string>("Audio", "AudioModFolders", "Demo",
+            @"Name of the active audio mod folder(s)
             For loading multiple texture mods add them comma seperated in ascending priority
             Example:
-                MusicMod1,MusicMod2,MusicMod3
-            MusicMod3 overwrites the soundfiles of MusicMod2 which overwrites the soundfiles of MusicMod1");
+                AudioMod1,AudioMod2,AudioMod3
+            AudioMod3 overwrites the soundfiles of AudioMod2 which overwrites the soundfiles of AudioMod1");
             // Config: General (args: section, key, default, description)
             ConfigEnableTextureMods = Config.Bind<bool>("General", "EnableTextureMods", true, "Set to true to enable texture mods, false to completely disable them");
             ConfigTextureModFolders = Config.Bind<string>("General", "TextureModFolders", "Demo",
@@ -60,7 +60,8 @@ namespace AssetReplacer
 
             if (!enableAllMods)
             {
-                Log.LogInfo("All mods are disabled in the config (BepInEx\\config\\AssetReplacer.cfg). No game assets will be replaced");
+
+                Log.LogInfo($"All mods are disabled in the config {Config.ConfigFilePath}. No game assets will be replaced");
                 return;
             }
 
@@ -77,40 +78,49 @@ namespace AssetReplacer
                     Log.LogInfo("Configured Texture Mod Directory: " + modFolder);
                     FileLoader.TextureModFolders.Add(modFolder);
                 }
+
                 TextureStore.Init();
+
+                try
+                {
+                    hPatchTitleCursor = Harmony.CreateAndPatchAll(typeof(Patch.PatchTitleCursor), "PatchTitleCursor");
+                    hPatchControllerStart = Harmony.CreateAndPatchAll(typeof(Patch.PatchControllerStart), "PatchControllerStart");
+                }
+                catch (Exception e)
+                {
+                    Log.LogError(e.Message);
+                    Log.LogError($"{PluginInfo.PLUGIN_GUID} failed to patch Textures.");
+                }
             }
             else
             {
-                Log.LogInfo("Texture mods are disabled in the config (BepInEx\\config\\AssetReplacer.cfg). Default game textures will be used");
+                Log.LogInfo($"Texture mods are disabled in the config {Config.ConfigFilePath}. Default game textures will be used");
             }
 
-            //Music
-            if (ConfigEnableMusicMods.Value)
+            //Audio
+            if (ConfigEnableAudioMods.Value)
             {
-                foreach (string modFolder in ConfigMusicModFolders.Value.Split(','))
+                foreach (string modFolder in ConfigAudioModFolders.Value.Split(','))
                 {
-                    Log.LogInfo("Configured Music Mod Directory: " + modFolder);
-                    FileLoader.MusicModFolders.Add(modFolder);
+                    Log.LogInfo("Configured Audio Mod Directory: " + modFolder);
+                    FileLoader.AudioModFolders.Add(modFolder);
                 }
+
                 AudioStore.Init();
 
+                try
+                {
+                    hPatchAudioSource = Harmony.CreateAndPatchAll(typeof(Patch.PatchAudioSource), "PatchAudioSource");
+                }
+                catch (Exception e)
+                {
+                    Log.LogError(e.Message);
+                    Log.LogError($"{PluginInfo.PLUGIN_GUID} failed to patch Audio.");
+                }
             }
             else
             {
-                Log.LogInfo("Music mods are disabled in the config (BepInEx\\config\\AssetReplacer.cfg). Default game music will be used");
-            }
-
-            try
-            {
-                hPatchTitleCursor = Harmony.CreateAndPatchAll(typeof(Patch.PatchTitleCursor), "PatchTitleCursor");
-                hPatchTitleStart = Harmony.CreateAndPatchAll(typeof(Patch.PatchTitleStart), "PatchTitleStart");
-                hPatchBattleStart = Harmony.CreateAndPatchAll(typeof(Patch.PatchBattleStart), "PatchBattleStart");
-                hPatchAudioSource = Harmony.CreateAndPatchAll(typeof(Patch.PatchAudioSource), "PatchAudioSource");
-            }
-            catch (Exception e)
-            {
-                Log.LogError(e.Message);
-                Log.LogError($"{PluginInfo.PLUGIN_GUID} failed to patch methods.");
+                Log.LogInfo($"Audio mods are disabled in the config {Config.ConfigFilePath}. Default game audio will be used");
             }
         }
     }
