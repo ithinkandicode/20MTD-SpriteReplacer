f0f9abe2cff5d2b016b1b2601636a27a1d2af0b9
Dragoon <DragoonX6@users.noreply.github.com>
2022-11-18 15:50:44 +0100

    Dynamically change the fog of war color

--- src/patches/PatchBattleStart.cs
+++ src/patches/PatchBattleStart.cs
@@ -1,5 +1,10 @@
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
+
+using flanne;
+
+using AssetReplacer.AssetStore;
 
 namespace AssetReplacer.Patch
 {
@@ -16,6 +21,50 @@ namespace AssetReplacer.Patch
                 foreach (Sprite sprite in sprites)
                 {
                     Utils.TryReplaceTexture2D(sprite);
+                }
+
+                if(AssetReplacer.ConfigTextureDynamicFogOfWar.Value)
+                {
+                    Transform fogOfWarCanvas = PlayerController.Instance.transform.Find("FogOfWarCanvas");
+
+                    if(fogOfWarCanvas != null)
+                    {
+                        Transform child = fogOfWarCanvas.GetChild(0);
+
+                        if(child == null)
+                        {
+                            Debug.LogError("Fog of war canvas has no child!");
+                            return;
+                        }
+
+                        RawImage img = child.GetComponent<RawImage>();
+
+                        if(img == null)
+                        {
+                            Debug.LogError("Fog of war image has no RawImage component!");
+                            return;
+                        }
+
+                        Texture2D grasstile = null;
+                        if(!TextureStore.textureDict.TryGetValue("T_TileGrass", out grasstile))
+                            return;
+
+                        Color[] colors = grasstile.GetPixels();
+
+                        Color additive = Color.black;
+
+                        foreach(Color color in colors)
+                        {
+                            additive += color;
+                        }
+
+                        Color average = additive / colors.Length * 0.75f;
+
+                        Material material = img.material;
+                        material.color = average;
+                    }
+                    else
+                        Debug.LogError("Unable to find fog of war canvas!");
                 }
             }
         }
